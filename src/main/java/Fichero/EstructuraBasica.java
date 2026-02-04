e4f19b6e9ae85086bd73ebb02c0b0a3adfc86442
Samuel <sss93860532@gmail.com>
2026-02-04 12:51:51 +0100

    Creacion ficheros en java

---
+++ src/main/java/Fichero/EstructuraBasica.java
@@ -0,0 +1,24 @@
+package Fichero;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EstructuraBasica {
+    public static void main(String[] args) {
+        FileWriter fw = null;
+            
+        try {
+            fw = new FileWriter("datos.txt");
+            
+            fw.write("Primera línea\n");
+            fw.write("Segunda línea\n");
+            fw.write("Tercera línea\n");
+            
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }finally{
+            
+        }        
+    }
+}
---
+++ src/main/java/Fichero/EstructuraCorrecta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Fichero;
+
+/**
+ * Hasta el final... vamos Real
+ * @author Kylian<16@ChampionLeague.com>
+ * @url realmadrid.com<realmadrid.com>
+ * @date 4 feb 2026
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+public class EstructuraCorrecta {
+
+}
